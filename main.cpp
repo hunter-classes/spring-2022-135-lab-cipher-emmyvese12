818626555a3e2eb1cbfe8fa889a9c6c92642f754
Emmy Veselinov <emmyveselinov@gmail.com>
2022-03-28 23:12:51 -0400

    added simple cout to test code

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <string>
 
 
 #include "funcs.h"
 
-int main()
-{
+int main(){
+  std::string str = "hello";
+  std::string result = solve(str);
+  std::cout << result;
+
+
   return 0;
 }
