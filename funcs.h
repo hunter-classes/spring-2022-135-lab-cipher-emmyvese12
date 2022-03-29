5b7a16dedabbbe6776eda2fcdcf69d906ed265c9
Emmy Veselinov <emmyveselinov@gmail.com>
2022-03-28 23:12:19 -0400

    added function declarations

--- funcs.h
+++ funcs.h
@@ -1,3 +1,16 @@
 #pragma once
 
 // add prototypes here
+char shiftChar(char c, int rshift);
+
+std::string encryptCaesar(std::string plaintext, int rshift);
+
+int indexOfLetter(char letter);
+
+double distance(double v1[], double v2[], int len);
+
+std::string solve(std::string encrypted_string);
+
+
+
+
